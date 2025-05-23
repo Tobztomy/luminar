d094fe80c602d5ac7d1e103d8b6000c50abe3d2a
TOBIN K TOMY <144670158+Tobztomy@users.noreply.github.com>
2025-05-23 11:41:31 +0530

    Add files via upload

---
+++ BitwiseDemo.java
@@ -0,0 +1,14 @@
+class BitwiseDemo {
+	public static void main(String args[]) {
+		int numOne=12;
+		int numTwo=25;
+		int numThree=5;
+		int numFour=5;
+		System.out.println(numOne & numTwo);
+		System.out.println(numOne | numTwo);
+		System.out.println(numOne ^ numTwo);
+		System.out.println(numThree << 2);
+		System.out.println(numFour >> 3);
+	}
+
+}

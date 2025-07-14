6b481e30ff28277bbaa7d2eb037cac9cb02615f3
TOBIN K TOMY <144670158+Tobztomy@users.noreply.github.com>
2025-07-14 18:09:08 +0530

    Add files via upload

---
+++ exam/module 3/StringReverseWithoutMethod.java
@@ -0,0 +1,15 @@
+package stringpack;
+
+public class StringReverseWithoutMethod {
+
+	public static void main(String[] args) {
+		String name="tobin";
+		System.out.print("Reverse of String "+name+" is ");
+		for(int i=name.length()-1;i>=0;i--){
+			System.out.print(name.charAt(i));
+		}
+		
+		
+	}
+
+}
